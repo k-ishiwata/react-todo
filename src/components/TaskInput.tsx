9ee55d825ca2776380e4f0516aff450587b064aa
k-ishiwata <ki.ishiwata@gmail.com>
2020-02-02 11:23:31 +0900

    Formバリデーション

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,34 +1,43 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch } from 'react-redux'
 import { addTask } from '../modules/tasksModule'
+import { useForm } from 'react-hook-form'
+
+type FormData = {
+    title: string
+}
 
 const TaskInput: React.FC = () => {
     const dispatch = useDispatch()
 
-    const [ inputTitle, setInputTitle ] = useState('')
+    const { register, handleSubmit, errors, reset } = useForm<FormData>()
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputTitle(e.target.value)
-    }
-
-    const handleSubmit = () => {
-        dispatch(addTask(inputTitle))
-        setInputTitle('')
+    const handleOnSubmit = (data: FormData) => {
+        dispatch(addTask(data.title))
+        reset()
     }
 
     return (
-        <div className="input-form">
+        <form onSubmit={handleSubmit(handleOnSubmit)} className="input-form">
             <div className="inner">
                 <input
                     type="text"
-                    className="input"
-                    value={inputTitle}
-                    onChange={handleInputChange}
+                    name="title"
+                    className={`input ${errors.title && 'error'}`}
                     placeholder="TODOを入力してください。"
+                    ref={register({
+                        required: 'タイトルは必ず入力してください。',
+                        minLength: { value: 3, message: 'タイトルは3文字以上で入力してください。'},
+                        maxLength: { value: 200, message: 'タイトルは200文字以下で入力してください。'}
+                    })}
                 />
-                <button onClick={handleSubmit} className="btn is-primary">追加</button>
+                <button className="btn is-primary">追加</button>
+                {
+                    errors.title &&
+                    <span className="error-message">{ errors.title.message }</span>
+                }
             </div>
-        </div>
+        </form>
     )
 }
 
